86f3b30871edbbdcb2d574c8ecea9eabf6813f74
Lili Fuentes <lile7886@gmail.com>
2020-12-12 10:04:46 -0500

    chefMoji

--- src/Components/Chefs.js
+++ src/Components/Chefs.js
@@ -5,19 +5,17 @@ import '../css/Chefs.css'
 class Chefs extends Component {
     render() {
         return (
-            <div>
+            <div className='chefs'>
                 <h1>CHEFS</h1>
                 <br/>
                 <section className='background'>
                     <span title="La Carte"><img className="chefMoji" alt="chefMoji" src="https://lfuen.github.io/laCarte/images/chefmoji.png"/></span>
                 </section>
-                <ul>
-                    <li>Chef Johnathan Middleton</li>
-                    <li>Chef Francesca Guiles</li>
-                    <li>Chef Joaquin Hernandez</li>
-                    <li>Chef Christian Hollander</li>
-                    <li>Chef Madelyn Staes</li>
-                </ul>
+                    <h4>Chef Johnathan Middleton</h4>
+                    <h4>Chef Francesca Guiles</h4>
+                    <h4>Chef Joaquin Hernandez</h4>
+                    <h4>Chef Christian Hollander</h4>
+                    <h4>Chef Madelyn Staes</h4>
             </div>
         )
     }
