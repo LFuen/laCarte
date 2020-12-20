1ec79ec95b4c14ffd48e4d067c5a10353894d0d6
Lili Fuentes <lile7886@gmail.com>
2020-12-20 18:42:45 -0500

    adding vercel and build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://limitless-castle-88524.herokuapp.com/',
+    API_ENDPOINT: 'https://limitless-castle-88524.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY
 }
 
