87b04bf2c20c118fbc48e1baf04c546ff4abf0fb
Lili Fuentes <lile7886@gmail.com>
2020-12-20 20:02:15 -0500

    new build after making api a const

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export default api = {
+const api = {
     API_ENDPOINT: 'http://localhost:3000',
     API_KEY: process.env.REACT_APP_API_KEY
 }
+
+export default api
 
 // for API ENDPOINT - https://limitless-castle-88524.herokuapp.com/
