c26416054a02cdeaee9755fe3bb8aa28a6be2822
Lili Fuentes <lile7886@gmail.com>
2020-12-28 21:47:10 -0500

    changed css for welcome component

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -7,7 +7,7 @@ import './Welcome.css'
 class Welcome extends Component {
     render() {
         return (
-            <div>
+            <div className='welcomePage'>
                 <span title="La Carte"><img className="titleImage" alt="LaCarte" src="https://lfuen.github.io/laCarte/images/laCarteFont.png"/></span> 
                 <h3 className='worked'>The Healthy-Meal-Alternative App for the Busy Professional.</h3>
                 <h5>(We didn't say workaholic, you did.)</h5>
